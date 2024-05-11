b20fc81d24ac21b4ab46d11d3aff27e3881f4ceb
YeltzynS <111905803+YeltzynS@users.noreply.github.com>
2024-05-10 19:30:01 -0500

    Add files via upload

---
+++ src/main/java/co/edu/eci/cvds/repository/ProductoRepository.java
@@ -0,0 +1,12 @@
+package co.edu.eci.cvds.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import co.edu.eci.cvds.model.Producto;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
+    Producto findByIdProducto(Integer id);
+
+}
---
+++ src/main/java/co/edu/eci/cvds/repository/RevisionRepository.java
@@ -0,0 +1,13 @@
+package co.edu.eci.cvds.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import co.edu.eci.cvds.model.Revision;
+
+@Repository
+public interface RevisionRepository extends JpaRepository<Revision, Integer> {
+
+    Revision findByIdRevision(Integer id);
+
+   
+}
