e3f05000ce7c5da7140e6350c5dbb5cd2e46b4f1
YeltzynS <111905803+YeltzynS@users.noreply.github.com>
2024-05-23 02:09:36 -0500

    Update Carrito.java

--- src/main/java/co/edu/eci/cvds/model/Carrito.java
+++ src/main/java/co/edu/eci/cvds/model/Carrito.java
@@ -1,127 +1,43 @@
-package co.edu.eci.cvds.model;
+package co.edu.eci.cvds.controller;
 
-import java.sql.Date;
+import co.edu.eci.cvds.model.Vehiculo;
+import co.edu.eci.cvds.service.VehiculoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
-import java.util.ArrayList;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+@RestController
+@RequestMapping("/vehiculos")
+public class VehiculoController {
 
+    @Autowired
+    private VehiculoService vehiculoService;
 
-@Entity
-@Table(name ="CARRITOS DE COMPRAS")
-
-public class Carrito {
-    @Id
-    @Column(name = "CLIENTE_ID")
-    private String idCliente;
-    @Column(name = "CARRITO_ID")
-    private String idCarrito;
-    @Column(name = "FECHA_DEL_PEDIDO", nullable = false)
-    private Date fechaPedido;
-    @Column(name = "ESTADO", nullable = false)
-    private String estado;
-    @Column(name = "TOTAL", nullable = false)
-    private double total;
-    @OneToMany(mappedBy = "CLIENTE_ID")
-    private List<Servicio> servicios;
-    @OneToMany(mappedBy = "CLIENTE_ID")
-    private List<Revision> revisiones;
-
-    
-    public Carrito() {
+    @PostMapping
+    public ResponseEntity<Vehiculo> createVehiculo(@RequestBody Vehiculo vehiculo) {
+        return ResponseEntity.ok(vehiculoService.saveVehiculo(vehiculo));
     }
 
-
-    public Carrito(String idCliente, String idCarrito, Date fechaPedido, String estado, double total) {
-        this.idCliente = idCliente;
-        this.idCarrito = idCarrito;
-        this.fechaPedido = fechaPedido;
-        this.estado = estado;
-        this.total = total;
-        this.servicios = new ArrayList<Servicio>();
-        this.revisiones = new ArrayList<Revision>();
+    @GetMapping("/{id}")
+    public ResponseEntity<Vehiculo> getVehiculoById(@PathVariable Integer id) {
+        Vehiculo vehiculo = vehiculoService.getVehiculoById(id);
+        if (vehiculo != null) {
+            return ResponseEntity.ok(vehiculo);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-
-    public String getIdCliente() {
-        return idCliente;
+    @GetMapping
+    public ResponseEntity<List<Vehiculo>> getAllVehiculos() {
+        return ResponseEntity.ok(vehiculoService.getAllVehiculos());
     }
 
-
-    public void setIdCliente(String idCliente) {
-        this.idCliente = idCliente;
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteVehiculo(@PathVariable Integer id) {
+        vehiculoService.deleteVehiculo(id);
+        return ResponseEntity.noContent().build();
     }
-
-
-    public String getIdCarrito() {
-        return idCarrito;
-    }
-
-
-    public void setIdCarrito(String idCarrito) {
-        this.idCarrito = idCarrito;
-    }
-
-
-    public Date getFechaPedido() {
-        return fechaPedido;
-    }
-
-
-    public void setFechaPedido(Date fechaPedido) {
-        this.fechaPedido = fechaPedido;
-    }
-
-
-    public String getEstado() {
-        return estado;
-    }
-
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-
-    public double getTotal() {
-        return total;
-    }
-
-
-    public void setTotal(double total) {
-        this.total = total;
-    }
-
-
-    public List<Servicio> getServicios() {
-        return servicios;
-    }
-
-
-    public void setServicios(List<Servicio> servicios) {
-        this.servicios = servicios;
-    }
-
-
-    public List<Revision> getRevisiones() {
-        return revisiones;
-    }
-
-
-    public void setRevisiones(List<Revision> revisiones) {
-        this.revisiones = revisiones;
-    }
-
-    
-   
-
-    
-    
 }
