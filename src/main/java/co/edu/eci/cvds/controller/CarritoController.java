c98227edc302e01788097032497ec34934e1e243
YeltzynS <111905803+YeltzynS@users.noreply.github.com>
2024-05-23 02:05:24 -0500

    Update CarritoController.java

--- src/main/java/co/edu/eci/cvds/controller/CarritoController.java
+++ src/main/java/co/edu/eci/cvds/controller/CarritoController.java
@@ -1,11 +1,10 @@
 package co.edu.eci.cvds.controller;
 
+import co.edu.eci.cvds.model.Carrito;
+import co.edu.eci.cvds.service.CarritoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import co.edu.eci.cvds.model.Carrito;
-import co.edu.eci.cvds.repository.CarritoRepository;
 
 import java.util.List;
 
@@ -14,54 +13,31 @@ import java.util.List;
 public class CarritoController {
 
     @Autowired
-    private CarritoRepository carritoRepository;
+    private CarritoService carritoService;
 
-    // Endpoint para obtener todos los carritos
-    @GetMapping("/")
-    public ResponseEntity<List<Carrito>> getAllCarritos() {
-        List<Carrito> carritos = carritoRepository.findAll();
-        return new ResponseEntity<>(carritos, HttpStatus.OK);
-    }
-
-    // Endpoint para obtener un carrito por su ID
-    @GetMapping("/{id}")
-    public ResponseEntity<Carrito> getCarritoById(@PathVariable String id) {
-        Carrito carrito = carritoRepository.findByIdCarrito(id);
-        if (carrito == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(carrito, HttpStatus.OK);
-    }
-
-    // Endpoint para crear un nuevo carrito
-    @PostMapping("/")
+    @PostMapping
     public ResponseEntity<Carrito> createCarrito(@RequestBody Carrito carrito) {
-        Carrito nuevoCarrito = carritoRepository.save(carrito);
-        return new ResponseEntity<>(nuevoCarrito, HttpStatus.CREATED);
+        return ResponseEntity.ok(carritoService.saveCarrito(carrito));
     }
 
-    // Endpoint para actualizar los datos de un carrito existente
-    @PutMapping("/{id}")
-    public ResponseEntity<Carrito> updateCarrito(@PathVariable String id, @RequestBody Carrito carritoDetails) {
-        Carrito carrito = carritoRepository.findByIdCarrito(id);
-        if (carrito == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping("/{id}")
+    public ResponseEntity<Carrito> getCarritoById(@PathVariable Integer id) {
+        Carrito carrito = carritoService.getCarritoById(id);
+        if (carrito != null) {
+            return ResponseEntity.ok(carrito);
+        } else {
+            return ResponseEntity.notFound().build();
         }
-        carrito.setFechaPedido(carritoDetails.getFechaPedido());
-        carrito.setEstado(carritoDetails.getEstado());
-        carrito.setTotal(carritoDetails.getTotal());
-        Carrito carritoActualizado = carritoRepository.save(carrito);
-        return new ResponseEntity<>(carritoActualizado, HttpStatus.OK);
     }
 
-    // Endpoint para eliminar un carrito por su ID
+    @GetMapping
+    public ResponseEntity<List<Carrito>> getAllCarritos() {
+        return ResponseEntity.ok(carritoService.getAllCarritos());
+    }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCarrito(@PathVariable String id) {
-        Carrito carrito = carritoRepository.findByIdCarrito(id);
-        if (carrito == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        carritoRepository.delete(carrito);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<Void> deleteCarrito(@PathVariable Integer id) {
+        carritoService.deleteCarrito(id);
+        return ResponseEntity.noContent().build();
     }
 }
