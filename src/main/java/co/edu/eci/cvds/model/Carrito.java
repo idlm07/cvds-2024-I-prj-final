94ee9bba6d1c41a4e7c27a56402d98ac316a2660
YeltzynS <111905803+YeltzynS@users.noreply.github.com>
2024-05-23 02:09:51 -0500

    Update Carrito.java

--- src/main/java/co/edu/eci/cvds/model/Carrito.java
+++ src/main/java/co/edu/eci/cvds/model/Carrito.java
@@ -1,43 +1,109 @@
-package co.edu.eci.cvds.controller;
+package co.edu.eci.cvds.model;
 
-import co.edu.eci.cvds.model.Vehiculo;
-import co.edu.eci.cvds.service.VehiculoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
-@RestController
-@RequestMapping("/vehiculos")
-public class VehiculoController {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
-    @Autowired
-    private VehiculoService vehiculoService;
+@Entity
+@Table(name = "CARRITOS_DE_COMPRAS") // Corrige el nombre de la tabla
+public class Carrito {
+    @Id
+    @Column(name = "CLIENTE_ID")
+    private String idCliente;
 
-    @PostMapping
-    public ResponseEntity<Vehiculo> createVehiculo(@RequestBody Vehiculo vehiculo) {
-        return ResponseEntity.ok(vehiculoService.saveVehiculo(vehiculo));
+    @Column(name = "CARRITO_ID")
+    private String idCarrito;
+
+    @Column(name = "FECHA_DEL_PEDIDO", nullable = false)
+    private Date fechaPedido;
+
+    @Column(name = "ESTADO", nullable = false)
+    private String estado;
+
+    @Column(name = "TOTAL", nullable = false)
+    private double total;
+
+    @OneToMany(mappedBy = "carrito")
+    private List<Revision> revisiones;
+
+    public Carrito() {
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Vehiculo> getVehiculoById(@PathVariable Integer id) {
-        Vehiculo vehiculo = vehiculoService.getVehiculoById(id);
-        if (vehiculo != null) {
-            return ResponseEntity.ok(vehiculo);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    public Carrito(String idCliente, String idCarrito, Date fechaPedido, String estado, double total) {
+        this.idCliente = idCliente;
+        this.idCarrito = idCarrito;
+        this.fechaPedido = fechaPedido;
+        this.estado = estado;
+        this.total = total;
+        this.revisiones = new ArrayList<>();
     }
 
-    @GetMapping
-    public ResponseEntity<List<Vehiculo>> getAllVehiculos() {
-        return ResponseEntity.ok(vehiculoService.getAllVehiculos());
+    public String getIdCliente() {
+        return idCliente;
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteVehiculo(@PathVariable Integer id) {
-        vehiculoService.deleteVehiculo(id);
-        return ResponseEntity.noContent().build();
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getIdCarrito() {
+        return idCarrito;
+    }
+
+    public void setIdCarrito(String idCarrito) {
+        this.idCarrito = idCarrito;
+    }
+
+    public Date getFechaPedido() {
+        return fechaPedido;
+    }
+
+    public void setFechaPedido(Date fechaPedido) {
+        this.fechaPedido = fechaPedido;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public List<Revision> getRevisiones() {
+        return revisiones;
+    }
+
+    public void setRevisiones(List<Revision> revisiones) {
+        this.revisiones = revisiones;
+    }
+
+    public List<Carrito> getProductos() {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'getProductos'");
+    }
+
+    public Integer getId() {
+       
+        throw new UnsupportedOperationException("Unimplemented method 'getId'");
+    }
+
+    public void setId(int i) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'setId'");
     }
 }
