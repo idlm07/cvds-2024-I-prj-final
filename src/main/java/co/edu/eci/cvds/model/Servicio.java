3575bad0a733c9b8119e58838f720d83042bfddf
YeltzynS <111905803+YeltzynS@users.noreply.github.com>
2024-05-23 02:13:38 -0500

    Update Servicio.java

--- src/main/java/co/edu/eci/cvds/model/Servicio.java
+++ src/main/java/co/edu/eci/cvds/model/Servicio.java
@@ -1,19 +1,8 @@
 package co.edu.eci.cvds.model;
 
+import jakarta.persistence.*;
 import java.sql.Date;
 import java.util.List;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-
 
 @Entity
 @Table(name = "SERVICIO")
@@ -22,32 +11,36 @@ public class Servicio {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "SERVICIO_ID")
     private Integer idServicio;
+
     @Column(name = "DESCRIPCION")
     private String descripcion;
+
     @Column(name = "PRECIO")
     private double precio;
+
     @Column(name = "FECHA_INICIO")
     private Date fechaInicio;
+
     @Column(name = "FECHA_FIN")
     private Date fechaFin;
+
     @ManyToOne
     @JoinColumn(name = "CARRITO_ID")
     private Carrito carrito;
+
     @ManyToMany
-    @JoinTable(name ="Servicio_Producto", joinColumns= @JoinColumn (name = "SERVICIO_ID"), inverseJoinColumns = @JoinColumn(name = "PRODUCTO_ID") )
+    @JoinTable(
+        name = "Servicio_Producto",
+        joinColumns = @JoinColumn(name = "SERVICIO_ID"),
+        inverseJoinColumns = @JoinColumn(name = "PRODUCTO_ID")
+    )
     private List<Producto> productos;
 
+    // Constructor vacío
+    public Servicio() {}
 
-
-    
-    public Servicio() {
-    }
-
-
-
-
-    public Servicio(Integer idServicio, String descripcion, double precio, Date fechaInicio, Date fechaFin,
-            Carrito carrito, List<Producto> productos) {
+    // Constructor con parámetros
+    public Servicio(Integer idServicio, String descripcion, double precio, Date fechaInicio, Date fechaFin, Carrito carrito, List<Producto> productos) {
         this.idServicio = idServicio;
         this.descripcion = descripcion;
         this.precio = precio;
@@ -57,105 +50,60 @@ public class Servicio {
         this.productos = productos;
     }
 
-
-
-
+    // Getters y Setters
     public Integer getIdServicio() {
         return idServicio;
     }
-
-
-
 
     public void setIdServicio(Integer idServicio) {
         this.idServicio = idServicio;
     }
 
-
-
-
     public String getDescripcion() {
         return descripcion;
     }
-
-
-
 
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
 
-
-
-
     public double getPrecio() {
         return precio;
     }
-
-
-
 
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
-
-
-
     public Date getFechaInicio() {
         return fechaInicio;
     }
-
-
-
 
     public void setFechaInicio(Date fechaInicio) {
         this.fechaInicio = fechaInicio;
     }
 
-
-
-
     public Date getFechaFin() {
         return fechaFin;
     }
-
-
-
 
     public void setFechaFin(Date fechaFin) {
         this.fechaFin = fechaFin;
     }
 
-
-
-
     public Carrito getCarrito() {
         return carrito;
     }
-
-
-
 
     public void setCarrito(Carrito carrito) {
         this.carrito = carrito;
     }
 
-
-
-
     public List<Producto> getProductos() {
         return productos;
     }
 
-
-
-
     public void setProductos(List<Producto> productos) {
         this.productos = productos;
     }
-
-
-    
-   
 }
